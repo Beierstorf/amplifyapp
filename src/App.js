308f914622922df94f43f33771640600a289b6ce
Jürgen Beierstorf <22167296+Beierstorf@users.noreply.github.com>
2020-08-17 22:37:48 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 //jshint esversion:6
 import React from 'react';
+import Amplify from 'aws-amplify'
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 function App() {
   return (
